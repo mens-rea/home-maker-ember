4c95d82dca1bab66661f0f431ae2b0c733d38d2b
mens-rea <juan.miguel.g.alvarez@gmail.com>
2021-01-03 17:01:36 +0800

    add print report button

--- app/controllers/reports/bills.js
+++ app/controllers/reports/bills.js
@@ -1,9 +1,14 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
+import { computed, action } from '@ember/object';
 
 export default class ReportsBillsController extends Controller {
   @computed('model')
   get totalBills() {
     return this.model.mapBy('amount').reduce((a, b) => a + b, 0);
+  }
+
+  @action
+  printReport() {
+    window.print();
   }
 }
