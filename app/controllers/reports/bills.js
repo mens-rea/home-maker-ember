4f7b55f69a6a9c55c9ac450ea567ffeba5a18f42
mens-rea <juan.miguel.g.alvarez@gmail.com>
2021-01-04 00:23:56 +0800

    add unpaid filter for bills report

--- app/controllers/reports/bills.js
+++ app/controllers/reports/bills.js
@@ -2,13 +2,24 @@ import Controller from '@ember/controller';
 import { computed, action } from '@ember/object';
 
 export default class ReportsBillsController extends Controller {
-  @computed()
+  isUnpaidShown = false;
+
+  @computed('model', 'isUnpaidShown')
   get totalBills() {
+    if (this.isUnpaidShown){
+      return this.model.mapBy('amount').reduce((a, b) => a + b, 0);
+    }
+
     return this.model.filterBy('isPaid', true).mapBy('amount').reduce((a, b) => a + b, 0);
   }
 
   @action
   printReport() {
     window.print();
+  }
+
+  @action
+  toggleUnpaid() {
+    this.toggleProperty('isUnpaidShown');
   }
 }
