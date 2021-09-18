84d99ba810caf6278ad4aabaad93a45d9bed8c88
mens-rea <juan.miguel.g.alvarez@gmail.com>
2021-09-18 15:36:57 +0800

    show all recipe list

--- app/controllers/menu.js
+++ app/controllers/menu.js
@@ -9,23 +9,46 @@ export default class MenuController extends Controller {
   modalsManager;
 
   @tracked recipes = A([]);
+  @tracked allRecipes = A([]);
 
   @action
   async showRecipes() {
     let recipeList = [];
     console.log(this.recipes);
 
-    for (const recipe of this.model.recipes) {
-      let rec = await this.store.peekRecord('recipe', recipe);
-
-      if (!rec) {
-        rec = await this.store.findRecord('recipe', recipe);
-        console.log("record doesn't exist yet");
+    if (this.model.recipes) {
+      for (const recipe of this.model.recipes) {
+        let rec = await this.store.peekRecord('recipe', recipe);
+  
+        if (!rec) {
+          rec = await this.store.findRecord('recipe', recipe);
+          console.log("record doesn't exist yet");
+        }
+        recipeList.push(rec);
       }
-      recipeList.push(rec);
     }
     
     this.recipes = recipeList;
     console.log(this.recipes);
-  } 
+
+    // load all recipes available so we can add more
+    this.allRecipes = await this.store.peekAll('recipe');
+
+    if (this.allRecipes.length == 0) {
+      console.log('does not exist yet');
+      this.allRecipes = await this.store.findAll('recipe');
+    }
+  }
+
+  @action
+  async addRecipe(recipe) {
+    let newList = this.recipes;
+
+    let rec = await this.store.peekRecord('recipe', recipe.id);
+    newList.push(rec);
+
+    this.recipes = newList;
+    this.model.recipes = newList;
+    this.model.save();
+  }
 }
